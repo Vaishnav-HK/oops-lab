0a52c290cf491c4735618a2a42507b84adcefba9
Vaishnav-HK <vnavhk@gmail.com>
2025-01-02 23:16:57 +0530

    Add files via upload

---
+++ ex2/greatestOfThree.java
@@ -0,0 +1,23 @@
+class Main{
+	public static void main(String[] args){
+		int a=334;
+		int b=290;
+		int c=2310;
+
+		if (a>b){
+			if(a>c){      
+  				System.out.println(a+" is the largest");
+			}
+		}
+		if (b>c){
+			if (b>a){
+				System.out.println(b+" is the largest");
+			}
+		}
+		if (c>a){
+			if(c>b){
+				System.out.println(c+" is the largset");
+			}
+		}
+	}
+}
