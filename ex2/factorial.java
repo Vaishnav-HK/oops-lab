5499aaca6daa82010cc72bd0d94c8465830a860f
Vaishnav-HK <vnavhk@gmail.com>
2025-01-07 13:57:06 +0530

    Add files via upload

---
+++ ex2/factorial.java
@@ -0,0 +1,13 @@
+class Main{
+	public static void main(String[] args){
+		int num=5;
+		int fac=1;
+		for(int i=1; i<=5;i++){
+			fac=fac*i;
+		}
+		System.out.print("factorial of ");
+		System.out.print(num);
+		System.out.print(" is ");
+		System.out.print(fac);
+	}
+}
---
+++ ex2/palindromeNumber.java
@@ -0,0 +1,13 @@
+class Main{
+	public static void main(String[] args){
+		int num=123;
+		int rev=0;
+		int digit=0;
+		while(num>0){
+			digit=num%10;
+			rev=rev*10+digit;
+			num=Math.floorDiv(num,10);
+			}
+		System.out.print(rev);
+	}
+}
