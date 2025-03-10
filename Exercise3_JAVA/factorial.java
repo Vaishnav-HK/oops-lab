cd2b4c5fe4570e3a5790028a6e4159e1e927925d
Vaishnav-HK <vnavhk@gmail.com>
2025-03-10 23:21:48 +0530

    Update factorial.java :D

--- Exercise3_JAVA/factorial.java
+++ Exercise3_JAVA/factorial.java
@@ -1,13 +1,23 @@
+import java.util.Scanner;
 class factorial{
 	public static void main(String[] args){
-		int num=5;
-		int fac=1;
-		for(int i=1; i<=5;i++){
-			fac=fac*i;
-		}
-		System.out.print("factorial of ");
-		System.out.print(num);
-		System.out.print(" is ");
-		System.out.print(fac);
+		Scanner scan=new Scanner(System.in);
+		calculate obj=new calculate();
+		System.out.print("Enter number to find factorial: ");
+		long num=scan.nextLong();
+		long fact=obj.fac(num);
+		System.out.println("factorial of "+num+" is "+fact);
+		
+		
+		
+	}
+}
+class calculate{
+	public long fac(long num){
+		long fac=1;
+		for(long i=1; i<=num;i++){
+				fac=fac*i;
+			}
+		return fac;
 	}
 }
