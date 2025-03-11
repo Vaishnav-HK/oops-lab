fe2f3d35dc64728f1b36d6c66b3de6a3d91c6e32
Vaishnav-HK <vnavhk@gmail.com>
2025-03-11 07:20:33 +0530

    Updated reverse.java again

--- Exercise3_JAVA/reverse.java
+++ Exercise3_JAVA/reverse.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 class reverse{
 	public static void main(String[] args){
 		Scanner scan=new Scanner(System.in);
+		System.out.print("Enter number to reverse: ");
 		int num=scan.nextInt();
 		int rev=0;
 		int digit=0;
