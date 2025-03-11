0373ada20d28d2c0001216e9ee6dd1843ce55f47
Vaishnav-HK <vnavhk@gmail.com>
2025-03-11 07:30:59 +0530

    Update noOfDigits.java

--- Exercise3_JAVA/noOfDigits.java
+++ Exercise3_JAVA/noOfDigits.java
@@ -1,6 +1,10 @@
+import java.util.Scanner;
+
 class noOfDigits{
 	public static void main(String[] args){
-		int num=548834;
+		Scanner scan=new Scanner(System.in);
+		System.out.print("Enter number: ");
+		int num=scan.nextInt();
 		int tnum=num;
 		int digits=0;
 		while (tnum>0){
