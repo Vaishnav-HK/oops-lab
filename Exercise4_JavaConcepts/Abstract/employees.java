93903c87e72655108e5fa99319a4255796487930
Vaishnav-HK <vnavhk@gmail.com>
2025-04-04 11:42:53 +0530

    Update employees.java

--- Exercise4_JavaConcepts/Abstract/employees.java
+++ Exercise4_JavaConcepts/Abstract/employees.java
@@ -1,33 +1,54 @@
-class employees{
-	public static void main(String[] args){
-		SoftwareEngineer empOBJ=new SoftwareEngineer();
-		empOBJ.set("Myname","Developer",5);
-		empOBJ.setField("Bug Fixing");
-		empOBJ.display();
-	}
+public class employees_abstract {
+    public static void main(String[] args) {
+        Employee emp1 = new FullTimeEmployee("Vaish", 4000);
+        Employee emp2 = new PartTimeEmployee("Peter Parker", 20, 80);
+
+        emp1.displayInfo();
+        System.out.println("Monthly Salary: $" + emp1.calculateSalary());
+
+        emp2.displayInfo();
+        System.out.println("Monthly Salary: $" + emp2.calculateSalary());
+    }
 }
 
-abstract class Employee{
-	String name;
-	String posting;
-	int experience;
-	public void set(String n, String p, int exp){
-		name=n;
-		posting=p;
-		experience=exp;
-	}
+abstract class Employee {
+    String name;
+    Employee(String name) {
+        this.name = name;
+    }
+
+    abstract double calculateSalary();
+
+    void displayInfo() {
+        System.out.println("Employee Name: " + name);
+    }
 }
 
-class SoftwareEngineer extends Employee{
-	String field;
-	public void setField(String f){
-		field=f;
-	}
-	public void display(){
-		System.out.println("Name: "+name);
-		System.out.println("Posting: "+posting);
-		System.out.println("Field: "+field);
-		System.out.println("Experience: "+experience+" years");	
-	}
+class FullTimeEmployee extends Employee {
+    double salary;
+
+    FullTimeEmployee(String name, double salary) {
+        super(name);
+        this.salary = salary;
+    }
+
+    double calculateSalary() {
+        return salary;
+    }
 }
-	
+
+class PartTimeEmployee extends Employee {
+    double hourlyRate;
+    int hoursWorked;
+
+    PartTimeEmployee(String name, double hourlyRate, int hoursWorked) {
+        super(name);
+        this.hourlyRate = hourlyRate;
+        this.hoursWorked = hoursWorked;
+    }
+
+    double calculateSalary() {
+        return hourlyRate * hoursWorked;
+    }
+}
+
